3e324cb35da4b3207f58fb6be06e7e99fb56b218
Roman02082006 <romanvelazquezepet20@gmail.com>
2025-06-16 03:46:36 -0300

    haciendo el diseño

--- src/App.js
+++ src/App.js
@@ -6,12 +6,43 @@ import { FaEllipsisV } from 'react-icons/fa';
 
 function App() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState (false);
 
   const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
 
   return (
     <div className="App">
-      <div className="dropdown-container">
+      <div className={`sidebar ${sidebarOpen ? 'open' : ""}`}>
+          <div className="menu-icon" onClick={toggleSidebar}>
+          <div className="bar"/>
+          <div className="bar"/>
+          <div className="bar"/>
+          
+        </div>
+      <div className="menu-items"> 
+        <div className="icon-square"/>
+        <div className="icon-square"/>
+        <div className="icon-circle"/>
+      
+        </div>
+        
+        <div className="bottom-section">
+        <div className="icon-circle"/>
+        <div className="input-placeholder"/>
+        <div className="icon-hex"/>
+
+        </div>
+      </div>
+
+      <div className="main-content">
+      <span>Pagina Principal Del Administrador</span> 
+        <div className="header">
+        <div className="icon-circle">
+        <div className="circle-white"/>
+        <div className="circle-dark"/>
+
+         <div className="dropdown-container">
         <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown} direction="down">
           <DropdownToggle caret={false} className="custom-toggle">
             <FaEllipsisV size={20} />
@@ -23,14 +54,31 @@ function App() {
             <DropdownItem>Acción 4</DropdownItem>
           </DropdownMenu>
         </Dropdown>
-        <div className="circulo">
-          
-          </div>;
-          <div className="circulo2">
-          
-          </div>;
+        </div>
+
+
+        </div>
       </div>
+        <div className="card">
+          <div className="card-header">
+            <div className="icon-circle"/>
+            <div className="icon-square"/>
+            <div className="icon-square"/>
+          </div>
+          <div className="card-body">
+            <div className="line"/>
+            <div className="line"/>
+            <div className="line"/>
+
+          </div>
+        </div>
+
+
+  
+
+  
     </div>
+  </div>
   );
 }
 
