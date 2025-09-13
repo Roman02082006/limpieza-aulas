b31302c3232064c23d601fb7660bc74c931e0ebb
JuliannaGomez7 <julianna.gomez@init-lab.com.ar>
2025-09-13 17:56:06 -0300

    Insertando nuevas carpetas

--- src/Hooks/usePorteros.jsx
+++ src/Hooks/usePorteros.jsx
@@ -7,36 +7,43 @@ console.log(data);
 //crea coleccion y leo ducumentos 
 
 const col1 = collection(db, "porteros");
-const q = query(co, orderBy,("apellido", "asc"))
-const snapq1 = await getDocs(q);
-const snapg1 = await getDocs (col);
+const q1 = query(co, orderBy,("apellido", "asc"))
+const snapq1 = await getDocs(q1);
+const snapg1 = await getDocs (col1);
 const data1 = snap.docs.map (d=> ({id: d.id, ...d.data()}));
-console.log(data);
+console.log(data1);
 
 const col2 = collection(db, "porteros");
 const q2 = query(co, orderBy,("nombre", "asc"))
 const snapq2 = await getDocs(q2);
 const snapg2 = await getDocs (col2);
 const data2 = snap.docs.map (d=> ({id: d.id, ...d.data()}));
-console.log(data);
+console.log(data2);
 
 const col3 = collection(db, "porteros");
 const q3 = query(co, orderBy,("dni", "asc"))
 const snapq3 = await getDocs(q3);
 const snapg3 = await getDocs (col3);
 const data3 = snap.docs.map (d=> ({id: d.id, ...d.data()}));
-console.log(data);
+console.log(data3);
 
 const col4 = collection(db, "porteros");
 const q4 = query(co, orderBy,("telefono", "asc"))
 const snapq4 = await getDocs(q4);
 const snapg4 = await getDocs (col4);
 const data4 = snap.docs.map (d=> ({id: d.id, ...d.data()}));
-console.log(data);
+console.log(data4);
 
 const col5 = collection(db, "porteros");
 const q5 = query(co, orderBy,("instituciones", "asc"))
 const snapq5 = await getDocs(q5);
 const snapg5 = await getDocs (col5);
 const data5 = snap.docs.map (d=> ({id: d.id, ...d.data()}));
-console.log(data);
+console.log(data5);
+
+function listenById(id, cb, errCb){
+    const ref = doc(db, "profesores", id);
+    return onSnapshot (ref, (d) => { 
+        cb (d.exists () ? {id: d.id, ...d.data()}:null);
+}, errCb);
+}
