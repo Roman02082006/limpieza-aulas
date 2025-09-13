808062feec091048508736df45a2ced1cf7fa59e
JuliannaGomez7 <julianna.gomez@init-lab.com.ar>
2025-09-13 20:29:53 -0300

    Agregando Login

---
+++ src/Componentes/Login.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { auth } from "../Firebase/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/panel"); // redirige al panel (admin o usuario)
+    } catch (error) {
+      alert("Error al iniciar sesión: " + error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Iniciar Sesión</h2>
+      <input
+        type="email"
+        placeholder="Correo"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Ingresar</button>
+    </form>
+  );
+};
+
+export default Login;
