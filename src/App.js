bc17ee82de9aef006d503fca43847252a97ba25b
LucasSanchez47 <lucassanchez47929@gmail.com>
2025-09-13 21:06:17 -0300

    modificando el firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 
@@ -52,6 +52,87 @@ const App = () => {
         <Route path="/Login_Usuario" element={<Login_Usuario />} />
         <Route path="/Registro_Admin" element={<Registro_Admin />} />
         <Route path="/Registro_Usuario" element={<Registro_Usuario />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;*/
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+
+// Componentes de login y registro
+import Login from "./Componentes/Login";
+import Register from "./Componentes/Register";
+import PrivateRoute from "./Componentes/PrivateRoute";
+
+
+import PanelAdminUsuario from "./Pages/PanelAdminUsuario";
+import PanelUsuario from "./Pages/PanelUsuario";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      {/* Botón para ADMIN */}
+      <button className="button-classroom" onClick={() => navigate("/panel_admin")}>
+        <div className="logo-wrapper">
+          <svg className="classroom-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <rect width="100%" height="100%" rx="4" fill="#1a73e8" />
+            <circle cx="12" cy="10" r="3" fill="white" />
+            <rect x="6" y="15" width="12" height="2" fill="white" />
+          </svg>
+          <svg className="crown" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+            <path fill="gold" d="M8 24L24 8l8 16 8-16 16 16-8 24H16z" />
+          </svg>
+        </div>
+      </button>
+
+      {/* Botón para USUARIO */}
+      <button className="button-classroom" onClick={() => navigate("/panel_usuario")}>
+        <div className="logo-wrapper">
+          <svg className="classroom-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <rect width="100%" height="100%" rx="4" fill="#1a73e8" />
+            <circle cx="12" cy="10" r="3" fill="white" />
+            <rect x="6" y="15" width="12" height="2" fill="white" />
+          </svg>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Página principal */}
+        <Route path="/" element={<Home />} />
+
+        {/* Rutas protegidas */}
+        <Route
+          path="/panel_admin"
+          element={
+            <PrivateRoute>
+              <PanelAdminUsuario tipo="admin" />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/panel_usuario"
+          element={
+            <PrivateRoute>
+              <PanelUsuario />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Login y registro */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
