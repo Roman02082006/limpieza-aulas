1b44d4c0cd29a5f539318d2ed3b5858289d4eb96
JuliannaGomez7 <julianna.gomez@init-lab.com.ar>
2025-09-13 21:07:39 -0300

    Modificando panel

--- src/Pages/PanelAdminUsuario.jsx
+++ src/Pages/PanelAdminUsuario.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import AltaAula from "../Componentes/AltaAula";
-import ListadoAulas from "../Componente/ListadoAulas";
+import ListadoAulas from "../Componentes/ListadoAulas";
 import AltaPersonal from "../Componentes/AltaPersonal";
 import ListadoPersonal from "../Componentes/ListadoPersonal";
 import AltaHorario from "../Componentes/AltaHorario";
 import ListadoHorarios from "../Componentes/ListadoHorarios";
 
 const PanelAdminUsuario = ({ tipo }) => {
+  const roles = ["preceptores", "porteros", "admin", "regencia"];
+
   return (
     <div>
       <h1>Gestión de Limpieza y Personal de Aulas</h1>
@@ -14,22 +16,18 @@ const PanelAdminUsuario = ({ tipo }) => {
       {tipo === "admin" && (
         <div>
           <h2>Panel Admin</h2>
+
           {/* Aulas */}
           <AltaAula />
           <ListadoAulas />
 
           {/* Personal */}
-          <AltaPersonal tipo="preceptores" />
-          <ListadoPersonal tipo="preceptores" />
-
-          <AltaPersonal tipo="porteros" />
-          <ListadoPersonal tipo="porteros" />
-
-          <AltaPersonal tipo="admin" />
-          <ListadoPersonal tipo="admin" />
-
-          <AltaPersonal tipo="regencia" />
-          <ListadoPersonal tipo="regencia" />
+          {roles.map((rol) => (
+            <div key={rol}>
+              <AltaPersonal tipo={rol} />
+              <ListadoPersonal tipo={rol} />
+            </div>
+          ))}
 
           {/* Horarios */}
           <AltaHorario />
