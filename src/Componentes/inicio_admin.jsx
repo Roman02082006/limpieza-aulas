e8a74ef2161f4361d9970c4b764c41562256d00d
LucasSanchez47 <lucassanchez47929@gmail.com>
2025-10-24 12:58:21 -0300

    Agregando componentes

---
+++ src/Componentes/inicio_admin.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./inicio_admin.css";
+
+const InicioUsuario = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="welcome-container">
+      <div className="form-box">
+        <h1>
+          Bienvenido a <br />
+          <span>Limpieza De Aulas</span>
+        </h1>
+
+        <div className="buttons">
+          <button className="btn login-btn" onClick={() => navigate("/Login_Usuario")}>
+            Login
+          </button>
+          <button
+            className="btn register-btn"
+            onClick={() => navigate("/Registro_Usuario")}
+          >
+            Registro
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InicioAdmin;
